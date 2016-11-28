841c5e9653f81bf182d30b6417f23f1a536d74c2
Lisa Koss <lisakoss@uw.edu>
2016-11-27 21:34:07 -0800

    Fix bug so that error messages for name/password fields show up correctly

--- src/TeamSignUp.js
+++ src/TeamSignUp.js
@@ -168,6 +168,7 @@ class RequiredInput extends React.Component {
 
   render() {
     var errors = this.validate(this.props.value); //need to validate again, but at least isolated
+    console.log(errors);
     var inputStyle = 'form-group';
     if(!errors.isValid) inputStyle += ' invalid';
 
@@ -178,7 +179,7 @@ class RequiredInput extends React.Component {
                 value={this.props.value}
                 onChange={(e) => this.handleChange(e)}
         />
-        {errors &&
+        {!errors.isValid &&
           <p className="help-block error-missing">{this.props.errorMessage}</p>
         }
       </div>
