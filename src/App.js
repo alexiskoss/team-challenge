9d1c1cc88a817281fe4c938c2551f7c8b5632c7b
Alexis Koss <alexis30@uw.edu>
2016-11-27 18:42:55 -0800

    Add basic page format and error styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import SignUpForm from './TeamSignUp';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Sign Up</h1>
-        <SignUpForm />
+        <div className="inner-container">
+          <h1>Sign Up</h1>
+          <h2>Our service is fun and awesome, but you must be 13 years old to join</h2>
+          <SignUpForm />
+        </div>
       </div>
     );
   }
