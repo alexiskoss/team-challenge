1430d312f9917b6677b8353926648926f80f0034
Alexis Koss <alexis30@uw.edu>
2016-11-28 18:52:49 -0800

    Add comments to reset tests, closes #6

--- src/App.test.js
+++ src/App.test.js
@@ -15,31 +15,31 @@ describe('<SignUpForm /> component', () => {
 
   beforeEach(() => {
     wrapper = mount(<SignUpForm />); //render component
-    wrapper.find('#resetButton').simulate('click');
+    wrapper.find('#resetButton').simulate('click'); //click the reset button
   })
 
   it('should reset the email field', () => {
     var emailField = wrapper.find('EmailInput').get(0); //email field
-    expect(emailField.props.value).toEqual('');
+    expect(emailField.props.value).toEqual(''); //checks to see if field is blank
   });
 
   it('should reset the name field', () => {
     var nameField = wrapper.find('RequiredInput').get(0); //name field
-    expect(nameField.props.value).toEqual('');
+    expect(nameField.props.value).toEqual(''); //checks to see if field is blank
   });
 
   it('should reset the date of birth field', () => {
     var dobField = wrapper.find('BirthdayInput').get(0); //date of birth field
-    expect(dobField.props.value).toEqual('');
+    expect(dobField.props.value).toEqual(''); //checks to see if field is blank
   });
 
   it('should reset the password field', () => {
     var passwordField = wrapper.find('RequiredInput').get(1); //password field
-    expect(passwordField.props.value).toEqual('');
+    expect(passwordField.props.value).toEqual(''); //checks to see if field is blank
   });
 
   it('should reset the password confirmation field', () => {
     var passwordConField = wrapper.find('PasswordConfirmationInput').get(0); //password confirmation field
-    expect(passwordConField.props.value).toEqual('');
+    expect(passwordConField.props.value).toEqual(''); //checks to see if field is blank
   });
 });
