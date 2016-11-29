c70958a8a227ddf781af195b1ed6f11f6dbed9c7
Lisa Koss <lisakoss@uw.edu>
2016-11-28 20:59:07 -0800

    Add password confirmation tests, closes #5

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {shallow, mount} from 'enzyme';
 import sinon from 'sinon';
-import {RequiredInput} from './TeamSignUp';
+import {RequiredInput, PasswordConfirmationInput} from './TeamSignUp';
 import SignUpForm from './TeamSignUp'
 
 it('renders without crashing', () => {
@@ -11,27 +11,52 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-/* Tests RequiredInput component for valid and invalid case. */
+/* Tests RequiredInput component for valid and invalid cases. */
 describe('<RequiredInput /> component', () => {
-  /* Determines that field has required input and returns true. */
+  /* Verifies that field has required input. */
   it('should check if name/password field has required input', () => {
     var validateSpy = sinon.spy(RequiredInput.prototype, 'validate');
     const wrapper = shallow(<RequiredInput value="display words" />); // input given to text field
 
     expect(validateSpy.called).toEqual(true); // check that the callback was executed
     expect(validateSpy.getCall(0).args[0]).toEqual("display words"); // checks that parameter given to the callback matches input
-    expect(validateSpy.returnValues[0].isValid).toEqual(true); // true; contains input
+    expect(validateSpy.returnValues[0].isValid).toEqual(true); // contains input
     validateSpy.restore();
   });
 
-  /* Determines that field does not have required input and returns false. */
+  /* Verifies that field does not have required input. */
   it('should check if name/password field shows appropriate error message', () => {
     var validateSpy = sinon.spy(RequiredInput.prototype, 'validate');
     const wrapper = shallow(<RequiredInput value="" />); // empty text field
 
     expect(validateSpy.called).toEqual(true); // check that the callback was executed
     expect(validateSpy.getCall(0).args[0]).toEqual(""); // checks that parameter given to the callback matches input
-    expect(validateSpy.returnValues[0].isValid).toEqual(false); // false; missing input
+    expect(validateSpy.returnValues[0].isValid).toEqual(false); // missing input
+    validateSpy.restore();
+  });
+});
+
+/* Tests PasswordConfirminationInput component for valid and invalid cases. */
+describe('<PasswordConfirmationInput /> component', () => {
+  /* Verifies that passwords match. */
+  it('should check if passwords match', () => {
+    var validateSpy = sinon.spy(PasswordConfirmationInput.prototype, 'validate');
+    const wrapper = shallow(<PasswordConfirmationInput value="testpassword" password="testpassword" />); // input given to pass & confirm fields
+
+    expect(validateSpy.called).toEqual(true); // check that the callback was executed
+    expect(validateSpy.getCall(0).args[0]).toEqual("testpassword"); // checks that parameter given to the confirm field matches password field
+    expect(validateSpy.returnValues[0].isValid).toEqual(true); // passwords match
+    validateSpy.restore();
+  });
+
+  /* Verifies that passwords do not match. */
+  it('should check if passwords do not match', () => {
+    var validateSpy = sinon.spy(PasswordConfirmationInput.prototype, 'validate');
+    const wrapper = shallow(<PasswordConfirmationInput value="PASS2" password="pass1" />); // input given to pass & confirm fields
+
+    expect(validateSpy.called).toEqual(true); // check that the callback was executed
+    expect(validateSpy.getCall(0).args[0]).not.toEqual("pass1"); // checks that parameter given to the confirm field doesn't match password field
+    expect(validateSpy.returnValues[0].isValid).toEqual(false); // passwords don't match
     validateSpy.restore();
   });
 });
